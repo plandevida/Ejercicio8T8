398325da7f4cd63f9a8cb91cab3372fd6b1ee169
plandevida <plandevida@planes.com>
2013-05-02 21:26:30 +0200

    Initial Commit

---
+++ Ejercicio8T8/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ejercicio8T8
+//
+//  Created by Dani on 02/05/13.
+//  Copyright (c) 2013 EDA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
